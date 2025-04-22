b068265be137e5454757ce68c3a6c7d507d02a99
Steven Cardona <stevencardona2001@gmail.com>
2025-04-21 21:57:35 -0500

    Add files via upload

---
+++ Ejercicio5.py
@@ -0,0 +1,3 @@
+totalCuenta=float(input("Digite el total de la cuenta: "))
+propina=int(input("Digite el porcentaje de la propina: "))
+print(totalCuenta*(propina/100))
---
+++ Ejercicio7.py
@@ -0,0 +1,8 @@
+num1=float(input("Por favor dame un número: "))
+num2=float(input("Por favor dame otro número: "))
+if num1==num2:
+    print("Los números son iguales")
+elif num1>num2:
+    print(num1,"es mayor")
+else:
+    print(num2,"es mayor")
