f23526bd21b99c1864862ac3500d2a3c03396d60
Steven Cardona <stevencardona2001@gmail.com>
2025-04-28 21:06:12 -0500

    Update Ejercicio-9.py

--- Repaso/Ejercicio-9.py
+++ Repaso/Ejercicio-9.py
@@ -1,7 +1,20 @@
-nota = float(input("Escribe la nota: "))
-if 5> nota:
-    print("Perdió")
-elif 5<= nota <7:
-    print("Aprobó")
-else:
-    print("Sobresaliente")
+while True:
+    
+    try:
+        nota = float(input("Escribe la nota (0 a 10): "))
+
+        if nota < 0 or nota > 10:
+            print("Debes ingresar una nota válida")
+            continue
+
+        if 5> nota:
+            print("Perdió")
+            break
+        elif 5<= nota <7:
+            print("Aprobó")
+            break
+        else:
+            print("Sobresaliente")
+            break
+    except ValueError:
+        print("Debes ingresar un valor numérico")
