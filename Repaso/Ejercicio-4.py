91fd702e5c8d7d8353c32406e3cceac196f1bc62
Steven Cardona <stevencardona2001@gmail.com>
2025-04-28 20:49:32 -0500

    Update Ejercicio-4.py

--- Repaso/Ejercicio-4.py
+++ Repaso/Ejercicio-4.py
@@ -3,11 +3,11 @@ while True:
         temperatura = float(input("Por favor dame la temperatura: "))      
         
         if temperatura >= 0:
-         print("No hace frío")
-         break
+            print("No hace frío")
+            break
         else:
-         print("Hace frío")
-         break
+            print("Hace frío")
+            break
     
     except ValueError:
-      print("Debes ingresar un valor numérico")
+        print("Debes ingresar un valor numérico")
