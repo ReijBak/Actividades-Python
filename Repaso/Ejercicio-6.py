549d065cdfc78eb91f5735938091881d55392899
Steven Cardona <stevencardona2001@gmail.com>
2025-04-28 20:28:37 -0500

    Update Ejercicio-6.py

---
+++ Repaso/Ejercicio-6.py
@@ -0,0 +1,17 @@
+while True:
+    try:
+        hora = int(input("Por favor dame la hora (formato 24h y sin minutos): "))      
+
+        if hora<0 or hora>23:
+          print("El valor debe ser mayor o igual a 0 y menor a 24")
+          continue
+        
+        if 12<hora<18:
+         print("Es hora de trabajar")
+         break
+        else:
+         print("No es hora de trabajar")
+         break
+    
+    except ValueError:
+      print("Debes ingresar un valor numérico")
