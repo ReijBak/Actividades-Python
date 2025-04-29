bc72de8e4aaab7456d35944c5db337c7b8514b05
Steven Cardona <stevencardona2001@gmail.com>
2025-04-28 20:19:30 -0500

    Update Ejercicio-5.py

---
+++ Repaso/Ejercicio-5.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        num1 = float(input("Por favor dame un número: "))      
+        num2 = float(input("Por favor dame otro número: "))
+
+        if num1 >= 0 and num2 >= 0:
+         print("Ambos son positivos")
+         break
+        else:
+         print("No son positivos")
+         break
+    
+    except ValueError:
+      print("Debes ingresar un valor numérico")
