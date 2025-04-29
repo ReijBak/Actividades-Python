819ac1e6139ba6931ba876b926203f5f5ab5703a
Steven Cardona <stevencardona2001@gmail.com>
2025-04-28 21:12:58 -0500

    Update Ejercicio-10.py

--- Repaso/Ejercicio-10.py
+++ Repaso/Ejercicio-10.py
@@ -1,11 +1,24 @@
-edad = float(input("Escribe la edad: "))
-if 0< edad <12:
-    print("Niño")
-elif 12<= edad <18:
-    print("Adolescente")
-elif 18<= edad <60:
-    print("Adulto")
-elif edad <60:
-    print("Anciano")
-else:
-    print("Edad inválida")
+while True:
+    
+    try:
+        edad = float(input("Escribe la edad: "))
+        
+        if edad < 0 or edad > 120:
+            print("El valor no está en el rango de edad")
+            continue
+
+        if 0< edad <12:
+            print("Niño")
+            break
+        elif 12<= edad <18:
+            print("Adolescente")
+            break
+        elif 18<=edad <60 :
+            print("Adulto")
+            break
+        elif edad >= 60:
+            print("Anciano")
+            break
+    
+    except ValueError:
+        print("Debes ingresar un valor numérico")
