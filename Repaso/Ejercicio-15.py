aa0e5e99721a889114cf2ae582a09cdccd4c98d3
Steven Cardona <stevencardona2001@gmail.com>
2025-04-28 21:41:55 -0500

    Update Ejercicio-15.py

---
+++ Repaso/Ejercicio-15.py
@@ -0,0 +1,8 @@
+palabras = ["Sol","Luna","Estrella","Planeta"]
+print("Lista sin ordenar:",palabras)
+
+palabras.sort()
+print("Lista en orden alfabético:",palabras)
+
+palabras.reverse()
+print("Lista en orden alfabético inverso:",palabras)
