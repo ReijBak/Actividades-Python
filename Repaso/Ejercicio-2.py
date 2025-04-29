578b94552bc6545e3145be451487068a881e9c44
Steven Cardona <stevencardona2001@gmail.com>
2025-04-28 19:46:50 -0500

    Update Ejercicio-2.py

--- Repaso/Ejercicio-2.py
+++ Repaso/Ejercicio-2.py
@@ -1,5 +1,5 @@
 color = input("Por favor dime un color: ")
-if color == "Rojo":
-    print("Ese es el color que esperaba")
+if color.lower() == "rojo":
+    print("Ese es el color")
 else:
     print("Ese no es el color que esperaba")
