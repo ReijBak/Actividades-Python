14ec2551eb25fc206f0956bb7a10e541af983d04
Steven Cardona <stevencardona2001@gmail.com>
2025-04-29 00:20:54 -0500

    Delete Ejercicio7.py

--- Ejercicio7.py
+++
@@ -1,8 +0,0 @@
-num1=float(input("Por favor dame un número: "))
-num2=float(input("Por favor dame otro número: "))
-if num1==num2:
-    print("Los números son iguales")
-elif num1>num2:
-    print(num1,"es mayor")
-else:
-    print(num2,"es mayor")
