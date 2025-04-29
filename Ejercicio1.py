1f6f3c84d0d9772ca455d9449b0f80c177891297
Steven Cardona <stevencardona2001@gmail.com>
2025-04-29 00:18:41 -0500

    Delete Ejercicio1.py

--- Ejercicio1.py
+++
@@ -1,9 +0,0 @@
-nombre = input("Por favor dame tu nombre: ")
-apellido = input("Por favor dame tu apellido: ")
-edad = int(input("Por favor dame tu edad: "))
-x=""
-if edad>17:
-  x="Eres es mayor de edad"
-else:
-  x="Eres es menor de edad"
-print(nombre," ",apellido+"\n",edad,"\n",x,sep="")
