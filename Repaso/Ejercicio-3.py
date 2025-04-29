2326ae56cce64c6418ec31adc2b26e9f70e3426b
Steven Cardona <stevencardona2001@gmail.com>
2025-04-28 20:08:49 -0500

    Update Ejercicio-3.py

--- Repaso/Ejercicio-3.py
+++ Repaso/Ejercicio-3.py
@@ -1,5 +1,17 @@
-edad = int(input("Por favor dame tu edad: "))
-if edad >= "18":
-    print("Puede votar")
-else:
-    print("No puede votar")
+while True:
+    try:
+        edad = int(input("Por favor dame tu edad: "))
+        
+        if edad <= 0 or edad >= 120:
+            print("La edad debe estar entre 0 y 120.")
+            continue
+        
+        if edad >= 18:
+         print("Puede votar")
+         break
+        else:
+         print("No puede votar")
+         break
+    
+    except ValueError:
+      print("Debes ingresar un valor numérico")
