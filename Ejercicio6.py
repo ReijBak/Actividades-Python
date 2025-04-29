9abee67bdfe75631fd3aa1392939ccfe6b9c0ece
Steven Cardona <stevencardona2001@gmail.com>
2025-04-29 00:20:38 -0500

    Delete Ejercicio6.py

--- Ejercicio6.py
+++
@@ -1,8 +0,0 @@
-num = 7
-numusr=float(input("Por favor dame un número: "))
-if numusr==num:
-    print("Su número es igual")
-elif numusr>num:
-    print("Su número es mayor")
-else:
-    print("Su número es menor")
