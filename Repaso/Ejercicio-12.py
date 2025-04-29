5a07edf95df07d314359b1d1af7ba038e1a41ab6
Steven Cardona <stevencardona2001@gmail.com>
2025-04-28 21:31:09 -0500

    Update Ejercicio-12.py

--- Repaso/Ejercicio-12.py
+++ Repaso/Ejercicio-12.py
@@ -1,4 +1,12 @@
-frutas = ["Fresa","Mora","Arroz"]
-quitar = input("Escriba la fruta a eliminar: ")
-frutas.remove(quitar)
-print(frutas)
+frutas = ["manzana","mango", "fresa","mora"]
+
+print("Lista de frutas: ",frutas)
+eliminar = input("¿Qué fruta deseas eliminar? ")
+
+if eliminar in frutas:
+    frutas.remove(eliminar)
+    print("Fruta eliminada.")
+else:
+    print("La fruta no está en la lista.")
+
+print("Lista actualizada:",frutas)
