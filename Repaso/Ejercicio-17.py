1bec304205beb19d0f345517163b24eae08c7f5f
Steven Cardona <stevencardona2001@gmail.com>
2025-04-29 16:50:41 -0500

    Update Ejercicio-17.py

---
+++ Repaso/Ejercicio-17.py
@@ -0,0 +1,6 @@
+nombres = ["Ana","Luis","Ana","Pedro","Ana"]
+
+nombre_buscado = input("Ingresa un nombre para contar: ")
+
+cantidad = nombres.count(nombre_buscado)
+print(f"El nombre {nombre_buscado} aparece {cantidad} veces.")
