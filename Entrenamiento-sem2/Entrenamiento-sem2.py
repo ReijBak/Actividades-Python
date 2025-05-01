8839d289b326fe694504acc3f47a6b0747d8f5ed
Steven Cardona <stevencardona2001@gmail.com>
2025-04-30 21:43:33 -0500

    Update Entrenamiento-sem2.py

--- Entrenamiento-sem2/Entrenamiento-sem2.py
+++ Entrenamiento-sem2/Entrenamiento-sem2.py
@@ -15,67 +15,92 @@
 # ======================================//======================================
 
 notas = []
+continuar=True
 
 def agregar_nota():
 
-    while True:
         
         try:
             nota = float(input("Digite la nota a agregar (0-100): "))
             if nota<0 or nota>100:
                 print("Valor fuera de rango, intente de nuevo")  
-                continue
 
             if nota>=70:
                 print("Aprobó")
-                break
             else:
                 print("reprobó")
-                break
+                
         except ValueError:
             print("Valor inválido, intente de nuevo")
     
-    while True:
+    
         
         guardar_nota = input("¿Desea guardar la nota en la lista?(Si/No)\n").lower()
 
         if guardar_nota=="si" or guardar_nota=="sí":
             notas.append(nota)
             print("La nota ha sido agregada")
-            break
+        
         elif guardar_nota=="no":
             print("La nota no ha sido agregada")
-            break
         else:
             print("Opción no válida, intente de nuevo")
         
-        
+def consultar_nota():
+
+    for i in range(len(notas)):
+        print(f"las nota {i+1}. es  {notas[i]}")
+
+def calcular_promedio():
+
+    suma = sum(notas)
+    if suma == 0:
+        print("El promedio de las notas es: 0")
+    else:
+        promedio = suma / len(notas)
+        print("El promedio de las notas es:",promedio)
+
+def comparar_notas():
+
     
 
-print(" ======================================//====================================== \n")
-print("¡Bienvenido!\n")
-print("\nSeleccione una de las siguientes opciones:\n")
-print("1. Agregar una nota.\n2. Calcular promedio de notas.")
-print("3. Comparar notas según una nota de referencia.\n4. Consultar la existencia de una nota.")
-print("5. Salir\n")
+def main():       
 
-opcion = int(input("Por favor digite la opción que desea elegir aquí: ---> "))
+    while True:
 
-while True:
-    if opcion==1:
-        print("Has elegido: Agregar una nota.")
-        agregar_nota()
-    elif opcion==2:
-        print("Has elegido: Calcular promedio de notas.")
-        calcular_promedio()
-    elif opcion==3:
-        print("Has elegido: Comparar notas según una nota de referencia.")
-        comparar_notas()
-    elif opcion==4:
-        print("Has elegido: Consultar la existencia de una nota")
-        consultar_nota()
-    elif opcion==5:
-        print("Saliendo del programa...")
-        break
-    else:
-        print("Opción no válida, intente nuevamente")
+        print(" ======================================//====================================== \n")
+        print("¡Bienvenido!\n")
+        print("\nSeleccione una de las siguientes opciones:\n")
+        print("1. Agregar una nota.\n2. Calcular promedio de notas.")
+        print("3. Comparar notas según una nota de referencia.\n4. Consultar la existencia de una nota.")
+        print("5. Salir\n")
+
+        opcion = int(input("Por favor digite la opción que desea elegir aquí: ---> "))
+
+        if opcion==1:
+            continuar=True
+            while continuar:
+                print("Has elegido: Agregar una nota.")
+                agregar_nota()
+                opc=input("Desea seguir agregando notas si/no: ?")
+                if opc=="no" or opc=="n":
+                    continuar=False
+        elif opcion==2:
+            print("Has elegido: Calcular promedio de notas.")
+            calcular_promedio()
+        elif opcion==3:
+            print("Has elegido: Comparar notas según una nota de referencia.")
+            comparar_notas()
+        elif opcion==4:
+            print("Has elegido: Consultar la existencia de una nota")
+            consultar_nota()
+        elif opcion==5:
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción no válida, intente nuevamente")
+
+
+
+if __name__ == '__main__':
+    main()
