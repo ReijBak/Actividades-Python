7b4c12a41558b890811e7b2a8948d5bfdaf1f200
Steven Cardona <stevencardona2001@gmail.com>
2025-05-02 21:38:26 -0500

    Update Entrenamiento-sem2.py

--- Entrenamiento-sem2/Entrenamiento-sem2.py
+++ Entrenamiento-sem2/Entrenamiento-sem2.py
@@ -14,12 +14,11 @@
 #
 # ======================================//======================================
 
-notas = []
-continuar=True
+notas = [] # Se crea la lista notas la cual guardará notas para las funcionalidades del programa.
 
-def agregar_nota():
+def agregar_nota(): # La definición de esta función será la utilizada para agregar notas a la lista.
 
-    while True:
+    while True: # Se pide la nota al usuario y gracias a este ciclo, nos aseguramos que esté dentro del rango válido.
 
         try:
             
@@ -33,24 +32,31 @@ def agregar_nota():
             else:
                 print("reprobó")
                 break
-        except ValueError:
-            print("Valor inválido, intente de nuevo")
-    
-    guardar_nota = input("¿Desea guardar la nota en la lista?(S/N)\n").lower()
-    
-    if guardar_nota=="s":
-        notas.append(nota)
-        print("La nota ha sido agregada")
-    
-    elif guardar_nota=="n":
-        print("La nota no ha sido agregada")
-    else:
-        print("Opción no válida, intente de nuevo")
+        except ValueError: # Esta sentencia ayuda a que el programa no se detenga cuando el usuario ingrese un valor que no sea numérico.
+            print("Valor inválido, intente de nuevo") # Al estar al final de un ciclo, esto hace que lo siguente sea volver a pedir la entrada hasta que sea válida.
+      
+    while True: # Se pregunta al usuario si desea guardar la nota ingresada.
+        # Esta estructura es similar a la anterior, solo que al no tratarse de una entrada numerica, no es necesario utilizar la sentencia Try ya que esta admite cualquier tipo de entrada.
         
-def calcular_promedio():
+        guardar_nota = input("¿Desea guardar la nota en la lista?(S/N)\n").lower()
+
+        if guardar_nota=="s":
+            notas.append(nota)
+            print("La nota ha sido agregada")
+            break
+
+        elif guardar_nota=="n":
+            print("La nota no ha sido agregada")
+            break
+        
+        else: # En este caso solo es necesario un camino en el condicional que indique que escribió una entrada no válida, el cual no rompa el ciclo para poder tomar de nuevo la entrada.
+            print("Opción no válida, intente de nuevo")
+        
+def calcular_promedio(): # La definición de esta función nos ayuda a calcular el promedio de las notas contenidas en la lista.
 
     print("1. Promedio de las notas guardadas.\n2. Importar lista para promediar.")
     opcion_menu2 = int(input("Por favor digite la opción que desea elegir aquí: ---> "))
+    # Se puede utilizar la lista ya guardada o importar una lista nueva.
 
     if opcion_menu2==1:
 
@@ -58,7 +64,7 @@ def calcular_promedio():
 
     elif opcion_menu2==2:
 
-        while True:
+        while True: # Estructura idéntica a la utilizada en el ingreso de notas solo que aquí ya de toman varias notas al mismo tiempo.
             entrada_notas = input("Por favor ingrese las notas separadas por comas:\n")
 
             try:
@@ -74,7 +80,23 @@ def calcular_promedio():
 
         promedio(user_notas_float)
 
-def promedio(notas):
+        while True: # Aquí se le permite al usuario guardar la nueva lista o no, la cual reemplazará la existente si hay alguna.
+
+            guardar_notas = input("¿Desea guardar la lista y reemplazar la existente?(S/N)\n").lower()
+
+            if guardar_notas=="s":
+                notas [:]= user_notas_float
+                print("La lista ha sido guardada")
+                break
+
+            elif guardar_notas=="n":
+                print("La lista no ha sido guardada")
+                break
+            
+            else: 
+                print("Opción no válida, intente de nuevo")
+
+def promedio(notas): # Función que se encarga de calcular el promedio de la lista que reciba (Ya sea la guardada o la que recién se ingresó).
     suma = sum(notas)
     if suma == 0:
         print("No hay notas para promediar")
@@ -85,17 +107,19 @@ def promedio(notas):
         
         
 
-def comparar_notas():
+def comparar_notas(): # La definición de esta función nos ayuda a comparar qué notas son mayores/menores o iguales a la nota que el usuario ingrese.
     nota_referencia = float(input("Por favor ingrese la nota que desea comparar: "))
 
+    # Se crean tres listas locales en donde se separan las notas de la lista inicial, según el parámetro ingresado.
     notas_mayores = [nota for nota in notas if nota > nota_referencia]
-    notas_menores = [nota for nota in notas if nota < nota_referencia]
+    notas_menores = [nota for nota in notas if nota < nota_referencia] 
     notas_iguales = [nota for nota in notas if nota == nota_referencia]
 
     cantidad_mayores = len(notas_mayores)
     cantidad_menores = len(notas_menores)
     cantidad_iguales = len(notas_iguales)
 
+    # Se informa al usuario que cantidad de notas son mayores, menores e iguales a la nota ingresada y posteriormente cuáles son.
     print(f"La cantidad de notas mayores que {nota_referencia} son {cantidad_mayores}")
     print(f"La cantidad de notas menores que {nota_referencia} son {cantidad_menores}")
     print(f"La cantidad de notas iguales a {nota_referencia} son {cantidad_iguales}")
@@ -103,48 +127,60 @@ def comparar_notas():
     print(f"Notas mayores: {notas_mayores}")
     print(f"Notas menores: {notas_menores}")
 
-def consultar_nota():
+def consultar_nota(): # La definición de esta función nos permite mostrar al ususario qué notas hay en la lista guardada.
 
-    for i in range(len(notas)):
+    for i in range(len(notas)): # Se utiliza esta estructura cíclica ya que queremos imprimir todas las notas contenidas sin importar la cantidad que sean.
         print(f"La nota {i+1}. es  {notas[i]}")
 
-def main():       
+def main():  # La definición de esta función hace que este sea el método principal, donde iniciará el programa.      
 
-    while True:
+# Menú de opciones en dónde el usuario elegirá que función desea hacer efectiva.
+
+    while True: # Esta estructura nos permite regresar el menú cada vez que el usuario termine de utilizar una funcionalidad.
 
         print(" ======================================//====================================== \n")
         print("¡Bienvenido!\n")
         print("\nSeleccione una de las siguientes opciones:\n")
-        print("1. Agregar una nota.\n2. Calcular promedio de notas.")
-        print("3. Comparar notas según una nota de referencia.\n4. Consultar la existencia de una nota.")
+        print("1. Agregar una nota individual.\n2. Calcular promedio de notas.")
+        print("3. Comparar notas según una nota de referencia.\n4. Consultar la notas de la lista guardada.")
         print("5. Salir\n")
 
-        opcion_menu1 = int(input("Por favor digite la opción que desea elegir aquí: ---> "))
-
-        if opcion_menu1==1:
+        opcion_menu1 =input("Por favor digite la opción que desea elegir aquí: ---> ")
+        if opcion_menu1=="1":
             continuar=True
-            
-            while continuar:
+            while continuar: #Esta sentencia hace que el usuario pueda reutilizar esta función sin necesidad de salir al menú inicial.
+                
                 print("Has elegido: Agregar una nota.")
                 agregar_nota()
-                
-                opcion1=input("¿Desea seguir agregando notas?(S/N)").lower()
-                if opcion1=="n":
-                    continuar=False
 
-        elif opcion_menu1==2:
+                while True:
+
+                    opcion1=input("¿Desea seguir agregando notas?(S/N)").lower()
+
+                    if opcion1=="n":
+                        continuar=False
+                        break
+
+                    elif opcion1=="s":
+                        continue
+
+                    else:
+                        print("Opción no válida, intente nuevamente")
+
+
+        elif opcion_menu1=="2":
             print("Has elegido: Calcular promedio de notas.")
             calcular_promedio()
 
-        elif opcion_menu1==3:
+        elif opcion_menu1=="3":
             print("Has elegido: Comparar notas según una nota de referencia.")
             comparar_notas()
 
-        elif opcion_menu1==4:
+        elif opcion_menu1=="4":
             print("Has elegido: Consultar la existencia de una nota")
             consultar_nota()
 
-        elif opcion_menu1==5:
+        elif opcion_menu1=="5":
             print("Saliendo del programa...")
             break
         
@@ -153,3 +189,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
